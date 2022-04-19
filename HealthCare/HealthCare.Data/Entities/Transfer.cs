9c231c3496fa5c77d6e77aaaa6096c7d05e078e4
tince250 <t.s.mihajlovic25@gmail.com>
2022-04-19 22:52:36 +0200

    Added the second half of API entities

---
+++ HealthCare/HealthCare.Data/Entities/Transfer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCare.Data.Entities
+{
+    [Table("transfer")]
+    internal class Transfer
+    {
+        [Column("room_id1")]
+        private int RoomFromId { get; set; }
+
+        [Column("equipment_id")]
+        private int EquipmentId { get; set; }
+
+        [Column("room_id")]
+        private int RoomToId { get; set; }
+
+        [Column("amount")]
+        private int Amount { get; set; }
+
+        [Column("time_transfered")]
+        private DateTime TransferTime { get; set; }
+
+        [Column("deleted")]
+        private bool isDeleted { get; set; }
+    }
+}
