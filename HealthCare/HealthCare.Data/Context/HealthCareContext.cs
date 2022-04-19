3e3870c7682c751095ae377a1bc5b1f48aa09a41
Bojan Mijanovic <mijanovic.boki.bojan@gmail.com>
2022-04-19 21:20:06 +0200

    Added initial code for API and first class - anamnesis

---
+++ HealthCare/HealthCare.Data/Context/HealthCareContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthCare.Data.Context
+{
+    public class HealthCareContext: DbContext
+    {
+        public HealthCareContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
