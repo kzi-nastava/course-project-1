e820dae2cce7282472b12ab129cede32f247be1b
tince250 <t.s.mihajlovic25@gmail.com>
2022-04-19 23:18:57 +0200

    Added the rest of the API entity classes

---
+++ HealthCare/HealthCare.Data/Entities/ExaminationApproval.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCare.Data.Entities
+{
+    [Table("examination_approval")]
+    internal class ExaminationApproval
+    {
+        [Column("state")]
+        private string State { get; set; }
+
+        [Column("Examination_Room_id")]
+        private int RoomId { get; set; }
+
+        [Column("Examination_Doctor_id")]
+        private int DoctorId { get; set; }
+
+        [Column("Examination_examination_started")]
+        private DateTime StartTime { get; set; }
+
+        [Column("Examination_Patient_id")]
+        private int PatientId { get; set; }
+
+        [Column("deleted")]
+        private bool isDeleted { get; set; }
+    }
+}
