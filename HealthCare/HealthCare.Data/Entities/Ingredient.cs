b64dad21f5814d205ed48072e340b98562817ee1
Bojan Mijanovic <mijanovic.boki.bojan@gmail.com>
2022-05-08 01:06:34 +0200

    Added classes for ingredient table

---
+++ HealthCare/HealthCare.Data/Entities/Ingredient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCare.Data.Entities
+{
+    [Table("ingredient")]
+    public class Ingredient
+    {
+        [Column("id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public decimal Id { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; }
+
+        [Column("allergen")]
+        public bool IsAllergen { get; set; }
+    }
+}
---
+++ HealthCare/HealthCare.Domain/Services/IngredientService.cs
@@ -0,0 +1,59 @@
+﻿using HealthCare.Data.Entities;
+using HealthCare.Domain.Interfaces;
+using HealthCare.Domain.Models;
+using HealthCare.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCare.Domain.Services
+{
+    public class IngredientService : IIngredientService
+    {
+        private IIngredientRepository _ingridientRepository;
+
+        public IngredientService(IIngredientRepository ingridientRepository)
+        {
+            _ingridientRepository = ingridientRepository;
+        }
+
+        private IngredientDomainModel parseToModel(Ingredient ingridient)
+        {
+            IngredientDomainModel ingridientModel = new IngredientDomainModel
+            {
+                Id = ingridient.Id,
+                IsAllergen = ingridient.IsAllergen,
+                Name = ingridient.Name
+            };
+            return ingridientModel;
+        }
+
+        private Ingredient parseFromModel(IngredientDomainModel ingridientModel)
+        {
+            Ingredient ingridient = new Ingredient
+            {
+                Id = ingridientModel.Id,
+                IsAllergen = ingridientModel.IsAllergen,
+                Name = ingridientModel.Name
+            };
+            return ingridient;
+        }
+
+        public async Task<IEnumerable<IngredientDomainModel>> GetAll()
+        {
+            IEnumerable<Ingredient> data = await _ingridientRepository.GetAll();
+            if (data == null)
+                return new List<IngredientDomainModel>();
+
+            List<IngredientDomainModel> results = new List<IngredientDomainModel>();
+            foreach (Ingredient item in data)
+            {
+                results.Add(parseToModel(item));
+            }
+
+            return results;
+        }
+    }
+}
