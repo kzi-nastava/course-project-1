0f9229645c8b16f0f7460f3161385c65b2344067
Cone3214 <nemanja.majstorovic3214@gmail.com>
2022-05-26 18:14:34 +0200

    Added entities

---
+++ HealthCare/HealthCare.Data/Entities/Question.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCare.Data.Entities
+{
+    public class Question
+    {
+        [Column("id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public decimal Id { get; set; }
+
+        [Column("question_text")]
+        public string QuestionText { get; set; }
+    }
+}
---
+++ HealthCare/HealthCare.Data/Entities/Survey.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCare.Data.Entities
+{
+    public class Survey
+    {
+        [Column("id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public decimal Id { get; set; }
+    }
+}
