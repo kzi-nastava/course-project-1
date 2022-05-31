c9f983747290288290352daa4498ed590604c475
Cone3214 <nemanja.majstorovic3214@gmail.com>
2022-05-31 18:34:45 +0200

    Added CRUD for drug ingredients

--- HealthCare/HealthCare.Domain/Interfaces/IDrugIngredientService.cs
+++ HealthCare/HealthCare.Domain/Interfaces/IDrugIngredientService.cs
@@ -1,8 +1,11 @@
+using HealthCare.Domain.DTOs;
 using HealthCare.Domain.Models;
 
 namespace HealthCare.Domain.Interfaces;
 
 public interface IDrugIngredientService : IService<DrugIngredientDomainModel>
 {
-    
+    public DrugIngredientDomainModel Create(DrugIngredientDTO dto);
+    DrugIngredientDomainModel Delete(decimal id);
+    DrugIngredientDomainModel Update(DrugIngredientDTO drugIngredientDTO);
 }
--- HealthCare/HealthCare.Domain/Services/DrugIngredientService.cs
+++ HealthCare/HealthCare.Domain/Services/DrugIngredientService.cs
@@ -1,13 +1,17 @@
 using HealthCare.Data.Entities;
+using HealthCare.Domain.DTOs;
 using HealthCare.Domain.Interfaces;
 using HealthCare.Domain.Models;
+using HealthCare.Repositories;
 
 namespace HealthCare.Domain.Services;
 
 public class DrugIngredientService : IDrugIngredientService
 {
-    public DrugIngredientService()
+    private readonly IDrugIngredientRepository _drugIngredientRepository;
+    public DrugIngredientService(IDrugIngredientRepository drugIngredientRepository)
     {
+        _drugIngredientRepository = drugIngredientRepository;
     }
 
     public static DrugIngredientDomainModel ParseToModel(DrugIngredient drugIngredient)
@@ -40,9 +44,54 @@ public class DrugIngredientService : IDrugIngredientService
         return drugIngredient;
     }
 
-    public Task<IEnumerable<DrugIngredientDomainModel>> GetAll()
+    public async Task<IEnumerable<DrugIngredientDomainModel>> GetAll()
     {
-        //TODO: Implement this
-        throw new NotImplementedException();
+        IEnumerable<DrugIngredient> drugIngredients = await _drugIngredientRepository.GetAll();
+        return ParseToModel(drugIngredients);
+    }
+
+    private IEnumerable<DrugIngredientDomainModel> ParseToModel(IEnumerable<DrugIngredient> drugIngredients)
+    {
+        List<DrugIngredientDomainModel> drugIngredientModels = new List<DrugIngredientDomainModel>();
+        foreach(DrugIngredient drugIngredient in drugIngredients)
+        {
+            drugIngredientModels.Add(ParseToModel(drugIngredient));
+        }
+        return drugIngredientModels;
+    }
+
+    public DrugIngredientDomainModel Create(DrugIngredientDTO dto)
+    {
+        DrugIngredient drugIngredient = new DrugIngredient
+        {
+            DrugId = dto.DrugId,
+            IngredientId = dto.IngredientId,
+            Amount = dto.Amount,
+        };
+        _drugIngredientRepository.Post(drugIngredient);
+        _drugIngredientRepository.Save();
+        return ParseToModel(drugIngredient);
+    }
+
+    public DrugIngredientDomainModel Delete(decimal id)
+    {
+        DrugIngredient drugIngredient = _drugIngredientRepository.Get(id);
+        drugIngredient.IsDeleted = true;
+        _drugIngredientRepository.Update(drugIngredient);
+        _drugIngredientRepository.Save();
+        return ParseToModel(drugIngredient);
+    }
+
+    public DrugIngredientDomainModel Update(DrugIngredientDTO drugIngredientDTO)
+    {
+        DrugIngredient drugIngredient = new DrugIngredient
+        {
+            DrugId = drugIngredientDTO.DrugId,
+            IngredientId = drugIngredientDTO.IngredientId,
+            Amount = drugIngredientDTO.Amount,
+        };
+        _drugIngredientRepository.Update(drugIngredient);
+        _drugIngredientRepository.Save();
+        return ParseToModel(drugIngredient);
     }
 }
